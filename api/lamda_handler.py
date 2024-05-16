cc9a8ea044eff56a472b87d431f29525ed16196b
Marko Gusic <markoggusic@gmail.com>
2024-05-16 13:20:22 +0200

    made lambda iac and config

--- api/lamda_handler.py
+++ api/lamda_handler.py
@@ -10,4 +10,21 @@ import urllib.parse
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.getenv("ARTISTS_TABLE"))
+    table = dynamodb.Table("ARTISTS_TABLE")
+
+    print(json.dumps(event, indent=2))
+
+    for record in event["Records"]:
+        log_new_artists(record)
+
+
+def log_new_artists(record):
+    if record["eventName"] == "INSERT":
+        new_image = record["dynamodb"]["NewImage"]
+        artist_name = new_image.get("ArtistName", {}).get("S")
+
+        if artist_name:
+            # Print the artist's name
+            print(f"New artist added: {artist_name}")
+        else:
+            print("Artist name not found in the new image")
